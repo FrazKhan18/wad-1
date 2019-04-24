a77decf0ff217e5f43e60e4e11a6266bcb7cf5e6
frazkhan18 <3729khan>
2019-04-23 22:55:04 -0700

    Class Activity

---
+++ lec 13/script.js
@@ -0,0 +1,20 @@
+var username=prompt("Username:");
+var password=prompt("Password:");
+
+var data={
+    user:"Fraz",
+    pass: "Khan"
+}
+
+var newsfeed= [{
+    user: "saad",
+    content: "Just passed my exam :)"}]
+
+{
+    user: "zain",
+    content: "Enjoying weather!"
+}];
+
+if (username == data.user && password==data.pass)
+{ console.log(newsfeed);
+}
