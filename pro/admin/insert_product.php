2c28b970d429adc3995a8051f5eea397e5945798
FrazKhan18 <40578766+FrazKhan18@users.noreply.github.com>
2019-06-11 23:07:59 -0700

    filter added | search

--- pro/admin/insert_product.php
+++ pro/admin/insert_product.php
@@ -1,34 +1,4 @@
 <!DOCTYPE html>
-<?php
-$con = mysqli_connect("localhost","root","","tech_box_db");
-if(!$con)
-    die("Connection failed");
-?>
-<?php
-
-if(isset($_POST['insert_pro'])){
-    //getting text data from the fields
-    $pro_title = $_POST['pro_title'];
-    $pro_cat = $_POST['pro_cat'];
-    $pro_brand = $_POST['pro_brand'];
-    $pro_price = $_POST['pro_price'];
-    $pro_desc = $_POST['pro_desc'];
-    $pro_keywords = $_POST['pro_keywords'];
-    $pro_image = $_FILES['pro_image']['name'];
-    $temp_image = $_FILES['pro_image']['temp_name'];
-
-    move_uploaded_file($temp_img,"product_images/".time().$pro_img);
-
-
-
-    $insert_product = "insert into products (pro_cat, pro_brand,pro_title,pro_price,pro_desc,pro_keywords,pro_image) 
-                  VALUES ('$pro_cat','$pro_brand','$pro_title','$pro_price','$pro_desc','$pro_keywords','$pro_image');";
-   /* $insert_pro = mysqli_query($con, $insert_product);
-    if($insert_pro){
-        header("location: ".$_SERVER['PHP_SELF']);
-    }*/
-}
-?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -48,7 +18,7 @@ if(isset($_POST['insert_pro'])){
 <div class="container-fluid">
     <h1 class="text-center my-4"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New </span>
         Product </h1>
-    <form action="" method="post" e>
+    <form action="" method="post" enctype="multipart/form-data">
         <div class="row">
             <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
                 <label for="pro_title" class="float-md-right"> <span class="d-sm-none d-md-inline"> Product </span>
@@ -74,15 +44,6 @@ if(isset($_POST['insert_pro'])){
                     </div>
                     <select class="form-control" id="pro_cat" name="pro_cat">
                         <option>Select Category</option>
-                        <?php
-                            $getCatsQuery = "select * from categories";
-                            $getCatsResult = mysqli_query($con,$getCatsQuery);
-                            while($row = mysqli_fetch_assoc($getCatsResult)){
-                                $cat_id = $row['cat_id'];
-                                $cat_title = $row['cat_title'];
-                                echo "<option value='$cat_id'>$cat_title</option>";
-                            }
-                        ?>
                     </select>
                 </div>
             </div>
@@ -99,15 +60,6 @@ if(isset($_POST['insert_pro'])){
                     </div>
                     <select class="form-control" id="pro_brand" name="pro_brand">
                         <option>Select Brand</option>
-                        <?php
-                            $getBrandsQuery = "select * from brands";
-                            $getBrandsResult = mysqli_query($con,$getBrandsQuery);
-                            while($row = mysqli_fetch_assoc($getBrandsResult)){
-                                $brand_id = $row['brand_id'];
-                                $brand_title = $row['brand_title'];
-                                echo "<option value='$brand_id'>$brand_title</option>";
-                            }
-                            ?>
                     </select>
                 </div>
             </div>
